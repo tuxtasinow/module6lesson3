fd231a449fba528cb24b5982ab142c5f28cb02db
Bakhtiyar Tukhtasinov <144336544+tuxtasinow@users.noreply.github.com>
2024-07-26 17:03:36 +0500

    Add files via upload

--- module6lesson3.py
+++ module6lesson3.py
@@ -1,29 +1,37 @@
 class Horse:
-    x_distance = 0
-    sound = 'Frr'
+
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frr'
 
     def run(self, dx):
-        if isinstance(dx, int):
+        if isinstance(dx, (int, float)):
             self.x_distance += dx
 
 
 class Eagle:
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
+
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
-        if isinstance(dy, int):
+        if isinstance(dy, (int, float)):
             self.y_distance += dy
 
 
 class Pegasus(Horse, Eagle):
 
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
+
     def move(self, dx, dy):
-        super().run(dx)
-        super().fly(dy)
+        self.run(dx)
+        self.fly(dy)
 
     def get_pos(self):
-        return (self.x_distance, self.y_distance)
+        return self.x_distance, self.y_distance
 
     def voice(self):
         print(self.sound)
